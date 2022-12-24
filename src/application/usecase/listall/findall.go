d7b21969cee83c07d21a938df4084e842eb32962
Gabriel Lopes <gabrielrrlopes@gmail.com>
2022-12-24 12:48:23 -0300

    Basic 'usecase findall' was implemented.

--- src/application/usecase/listall/findall.go
+++ src/application/usecase/listall/findall.go
@@ -1,5 +1,24 @@
 package listall
 
-func NewFindAll()  {
-  
+import (
+	"fmt"
+
+	"github.com/grrlopes/go-moneyhoney/src/domain/entity"
+	"github.com/grrlopes/go-moneyhoney/src/domain/repository"
+)
+
+type execute struct {
+	findRepository repository.IMoneyRepo
+}
+
+func NewFindAll(repo repository.IMoneyRepo) Input {
+	return execute{
+		findRepository: repo,
+	}
+
+}
+
+func (e execute) Execute(req entity.Income) {
+	fmt.Println(req.Author)
+	e.findRepository.Save()
 }
--- src/server.go
+++ src/server.go
@@ -4,7 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/grrlopes/go-moneyhoney/src/application/usecase/listall"
 	"github.com/grrlopes/go-moneyhoney/src/domain/entity"
+	"github.com/grrlopes/go-moneyhoney/src/domain/repository"
+	"github.com/grrlopes/go-moneyhoney/src/infra/repositories/couchdb"
+)
+
+var (
+	repositories repository.IMoneyRepo = couchdb.NewMoneyRepository()
+	usecase      listall.Input         = listall.NewFindAll(repositories)
 )
 
 func main() {
@@ -15,6 +23,8 @@ func main() {
 
 		income := entity.Income{}
 		income.SetAuthor("xxxxx")
+
+		usecase.Execute(income)
 
 		c.JSON(http.StatusOK, gin.H{
 			"message": "blabla",
